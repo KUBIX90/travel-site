a704b973dadd98c94023cd75e63b4468870e1c5c
Mark <mark.j.kubik@gmail.com>
2017-06-27 16:41:40 +0100

    Applied lazyloading to images

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 module.exports = {
 	entry: {
-		App: "./app/assets/scripts/App.js"
+		App: "./app/assets/scripts/App.js",
+		Vendor: "./app/assets/scripts/Vendor.js"
 	},
 	output: {
 	   path: __dirname + "/app/temp/scripts",
-		filename: "App.js"
+		filename: "[name].js"
 	},
 	module:{
 		loaders:[
